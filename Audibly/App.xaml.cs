b65b4c47782ccc2cf4ff7302ef75d2aafecc24a4
Ryan Stewart <70178227+rstewa35@users.noreply.github.com>
2022-02-28 21:51:03 -0500

    init commit

---
+++ Audibly/App.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using WinRT.Interop;
+using static PInvoke.User32;
+
+namespace Audibly;
+
+/// <summary>
+///     Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Application
+{
+    private Window _mWindow;
+
+    /// <summary>
+    ///     Initializes the singleton application object.  This is the first line of authored code
+    ///     executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    /// <summary>
+    ///     Invoked when the application is launched normally by the end user.  Other entry points
+    ///     will be used such as when the application is launched to open a specific file.
+    /// </summary>
+    /// <param name="args">Details about the launch request and process.</param>
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        _mWindow = new MainWindow();
+        var hwnd = WindowNative.GetWindowHandle(_mWindow);
+
+        SetWindowDetails(hwnd, 515, 700);
+        _mWindow.Activate();
+    }
+
+    private static void SetWindowDetails(IntPtr hwnd, int width, int height)
+    {
+        var dpi = GetDpiForWindow(hwnd);
+        var scalingFactor = (float) dpi / 96;
+        width = (int) (width * scalingFactor);
+        height = (int) (height * scalingFactor);
+
+        _ = SetWindowPos(hwnd, SpecialWindowHandles.HWND_TOP,
+            0, 0, width, height,
+            SetWindowPosFlags.SWP_NOMOVE);
+        _ = SetWindowLong(hwnd,
+            WindowLongIndexFlags.GWL_STYLE,
+            (SetWindowLongFlags) (GetWindowLong(hwnd,
+                                      WindowLongIndexFlags.GWL_STYLE) &
+                                  // ~(int)SetWindowLongFlags.WS_MINIMIZEBOX & // disables minimize button on window
+                                  ~(int) SetWindowLongFlags.WS_MAXIMIZEBOX)); // disables maximize button on window
+
+        // disable resizing of the main window
+        var myWndId = Win32Interop.GetWindowIdFromWindow(hwnd);
+        var apw = AppWindow.GetFromWindowId(myWndId);
+        var presenter = apw.Presenter as OverlappedPresenter;
+
+        presenter!.IsResizable = false;
+    }
+}
---
+++ Audibly/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Storage.Pickers;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using WinRT.Interop;
+
+namespace Audibly;
+
+/// <summary>
+///     An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        // play/pause button disabled until an audio file is successfully opened
+        PlayPauseButton.IsEnabled = false;
+    }
+
+    private void AppBarButton_Click(object sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine("commandbar button pressed");
+    }
+
+    private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+    {
+        if ((string) PlayPauseButton.Tag == "play")
+        {
+            PlayPauseButton.Icon = new SymbolIcon(Symbol.Pause);
+            PlayPauseButton.Label = "Pause";
+            PlayPauseButton.Tag = "pause";
+        }
+        else
+        {
+            PlayPauseButton.Icon = new SymbolIcon(Symbol.Play);
+            PlayPauseButton.Label = "Play";
+            PlayPauseButton.Tag = "play";
+        }
+    }
+
+    private async void OpenAudioFile(object sender, RoutedEventArgs e)
+    {
+        // Create the file picker
+        var filePicker = new FileOpenPicker();
+
+        // Get the current window's HWND by passing in the Window object
+        var hwnd = WindowNative.GetWindowHandle(this);
+
+        // Associate the HWND with the file picker
+        InitializeWithWindow.Initialize(filePicker, hwnd);
+
+        // Use file picker like normal!
+        filePicker.FileTypeFilter.Add(".m4a");
+        var file = await filePicker.PickSingleFileAsync();
+
+        Debug.WriteLine($"[Class=MainWindow][Method=SetLocalMedia] file name: {file.Name}");
+    }
+}

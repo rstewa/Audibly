682b5e85e8d8552ea8251757e71528f5da8ba075
rstewa <70178227+rstewa@users.noreply.github.com>
2024-03-22 13:38:26 -0500

    made entire title bar draggable

--- Audibly.App/AppShell.xaml.cs
+++ Audibly.App/AppShell.xaml.cs
@@ -1,141 +1,125 @@
-﻿using System;
+﻿// Author: rstewa · https://github.com/rstewa
+// Created: 3/5/2024
+// Updated: 3/22/2024
+
+using System;
 using Windows.System;
 using Audibly.App.Views;
-using Microsoft.UI.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
-using muxc = Microsoft.UI.Xaml.Controls;
 
-namespace Audibly.App
+namespace Audibly.App;
+
+/// <summary>
+///     The "chrome" layer of the app that provides top-level navigation with
+///     proper keyboarding navigation.
+/// </summary>
+public sealed partial class AppShell : Page
 {
     /// <summary>
-    /// The "chrome" layer of the app that provides top-level navigation with
-    /// proper keyboarding navigation.
+    ///     Initializes a new instance of the AppShell, sets the static 'Current' reference,
+    ///     adds callbacks for Back requests and changes in the SplitView's DisplayMode, and
+    ///     provide the nav menu list with the data to display.
     /// </summary>
-    public sealed partial class AppShell : Page
+    public AppShell()
     {
-        /// <summary>
-        /// Initializes a new instance of the AppShell, sets the static 'Current' reference,
-        /// adds callbacks for Back requests and changes in the SplitView's DisplayMode, and
-        /// provide the nav menu list with the data to display.
-        /// </summary>
-        public AppShell()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            Loaded += (sender, args) =>
-            {
+        Loaded += (sender, args) => { NavView.SelectedItem = AudiobookListMenuItem; };
+
+        // Set up custom title bar.
+        App.Window.ExtendsContentIntoTitleBar = true;
+    }
+
+    /// <summary>
+    ///     Gets the navigation frame instance.
+    /// </summary>
+    public Frame AppFrame => frame;
+
+    /// <summary>
+    ///     Default keyboard focus movement for any unhandled keyboarding
+    /// </summary>
+    private void AppShell_KeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        var direction = FocusNavigationDirection.None;
+        switch (e.Key)
+        {
+            case VirtualKey.Left:
+            case VirtualKey.GamepadDPadLeft:
+            case VirtualKey.GamepadLeftThumbstickLeft:
+            case VirtualKey.NavigationLeft:
+                direction = FocusNavigationDirection.Left;
+                break;
+            case VirtualKey.Right:
+            case VirtualKey.GamepadDPadRight:
+            case VirtualKey.GamepadLeftThumbstickRight:
+            case VirtualKey.NavigationRight:
+                direction = FocusNavigationDirection.Right;
+                break;
+
+            case VirtualKey.Up:
+            case VirtualKey.GamepadDPadUp:
+            case VirtualKey.GamepadLeftThumbstickUp:
+            case VirtualKey.NavigationUp:
+                direction = FocusNavigationDirection.Up;
+                break;
+
+            case VirtualKey.Down:
+            case VirtualKey.GamepadDPadDown:
+            case VirtualKey.GamepadLeftThumbstickDown:
+            case VirtualKey.NavigationDown:
+                direction = FocusNavigationDirection.Down;
+                break;
+        }
+
+        if (direction != FocusNavigationDirection.None &&
+            FocusManager.FindNextFocusableElement(direction) is Control control)
+        {
+            control.Focus(FocusState.Keyboard);
+            e.Handled = true;
+        }
+    }
+
+    public readonly string AudiobookListLabel = "Audiobooks";
+
+    /// <summary>
+    ///     Navigates to the page corresponding to the tapped item.
+    /// </summary>
+    private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+    {
+        var pageType = args.IsSettingsInvoked ? typeof(SettingsPage) : typeof(AudiobookListPage);
+        if (pageType != null && pageType != AppFrame.CurrentSourcePageType) AppFrame.Navigate(pageType);
+    }
+
+    /// <summary>
+    ///     Ensures the nav menu reflects reality when navigation is triggered outside of
+    ///     the nav menu buttons.
+    /// </summary>
+    private void OnNavigatingToPage(object sender, NavigatingCancelEventArgs e)
+    {
+        if (e.NavigationMode == NavigationMode.Back)
+        {
+            if (e.SourcePageType == typeof(AudiobookListPage))
                 NavView.SelectedItem = AudiobookListMenuItem;
-            };
-
-            // Set up custom title bar.
-            App.Window.ExtendsContentIntoTitleBar = true;
-            // Set XAML element as a draggable region.
-            App.Window.SetTitleBar(AppTitleBar);
-            AppTitle.Text = Windows.ApplicationModel.Package.Current.DisplayName;
-
-            var nonClientInputSource = InputNonClientPointerSource.GetForWindowId(App.Window.AppWindow.Id);
+            else if (e.SourcePageType == typeof(SettingsPage)) NavView.SelectedItem = NavView.SettingsItem;
         }
+    }
 
-        /// <summary>
-        /// Gets the navigation frame instance.
-        /// </summary>
-        public Frame AppFrame => frame;
+    /// <summary>
+    ///     Invoked when the View Code button is clicked. Launches the repo on GitHub.
+    /// </summary>
+    private async void ViewCodeNavPaneButton_Tapped(object sender, TappedRoutedEventArgs e)
+    {
+        await Launcher.LaunchUriAsync(new Uri("https://github.com/rstewa/audibly"));
+    }
 
-        /// <summary>
-        /// Default keyboard focus movement for any unhandled keyboarding
-        /// </summary>
-        private void AppShell_KeyDown(object sender, KeyRoutedEventArgs e)
-        {
-            FocusNavigationDirection direction = FocusNavigationDirection.None;
-            switch (e.Key)
-            {
-                case VirtualKey.Left:
-                case VirtualKey.GamepadDPadLeft:
-                case VirtualKey.GamepadLeftThumbstickLeft:
-                case VirtualKey.NavigationLeft:
-                    direction = FocusNavigationDirection.Left;
-                    break;
-                case VirtualKey.Right:
-                case VirtualKey.GamepadDPadRight:
-                case VirtualKey.GamepadLeftThumbstickRight:
-                case VirtualKey.NavigationRight:
-                    direction = FocusNavigationDirection.Right;
-                    break;
-
-                case VirtualKey.Up:
-                case VirtualKey.GamepadDPadUp:
-                case VirtualKey.GamepadLeftThumbstickUp:
-                case VirtualKey.NavigationUp:
-                    direction = FocusNavigationDirection.Up;
-                    break;
-
-                case VirtualKey.Down:
-                case VirtualKey.GamepadDPadDown:
-                case VirtualKey.GamepadLeftThumbstickDown:
-                case VirtualKey.NavigationDown:
-                    direction = FocusNavigationDirection.Down;
-                    break;
-            }
-
-            if (direction != FocusNavigationDirection.None &&
-                FocusManager.FindNextFocusableElement(direction) is Control control)
-            {
-                control.Focus(FocusState.Keyboard);
-                e.Handled = true;
-            }
-        }
-
-        public readonly string AudiobookListLabel = "Audiobooks";
-
-        /// <summary>
-        /// Navigates to the page corresponding to the tapped item.
-        /// </summary>
-        private void NavigationView_ItemInvoked(muxc.NavigationView sender, muxc.NavigationViewItemInvokedEventArgs args)
-        {
-            var pageType = args.IsSettingsInvoked ? typeof(SettingsPage) : typeof(AudiobookListPage);
-            if (pageType != null && pageType != AppFrame.CurrentSourcePageType)
-            {
-                AppFrame.Navigate(pageType);
-            }
-        }
-
-        /// <summary>
-        /// Ensures the nav menu reflects reality when navigation is triggered outside of
-        /// the nav menu buttons.
-        /// </summary>
-        private void OnNavigatingToPage(object sender, NavigatingCancelEventArgs e)
-        {
-            if (e.NavigationMode == NavigationMode.Back)
-            {
-                if (e.SourcePageType == typeof(AudiobookListPage))
-                {
-                    NavView.SelectedItem = AudiobookListMenuItem;
-                }
-                else if (e.SourcePageType == typeof(SettingsPage))
-                {
-                    NavView.SelectedItem = NavView.SettingsItem;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Invoked when the View Code button is clicked. Launches the repo on GitHub. 
-        /// </summary>
-        private async void ViewCodeNavPaneButton_Tapped(object sender, TappedRoutedEventArgs e) =>
-            await Launcher.LaunchUriAsync(new Uri("https://github.com/rstewa/audibly"));
-
-        /// <summary>
-        /// Navigates the frame to the previous page.
-        /// </summary>
-        private void NavigationView_BackRequested(muxc.NavigationView sender, muxc.NavigationViewBackRequestedEventArgs args)
-        {
-            if (AppFrame.CanGoBack)
-            {
-                AppFrame.GoBack();
-            }
-        }
+    /// <summary>
+    ///     Navigates the frame to the previous page.
+    /// </summary>
+    private void NavigationView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+    {
+        if (AppFrame.CanGoBack) AppFrame.GoBack();
     }
 }

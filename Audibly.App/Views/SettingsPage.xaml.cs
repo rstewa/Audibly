bcd99e9af3c9b9fd31b07ef507e9e49b80e3cc29
rstewa <70178227+rstewa@users.noreply.github.com>
2024-04-24 13:44:54 -0500

    added assemblyinfo

--- Audibly.App/Views/SettingsPage.xaml.cs
+++ Audibly.App/Views/SettingsPage.xaml.cs
@@ -24,7 +24,10 @@ public sealed partial class SettingsPage : Page
     {
         get
         {
-            var version = Assembly.GetEntryAssembly().GetName().Version;
+            var assembly = Assembly.GetEntryAssembly();
+            if (assembly == null) return string.Empty;
+            var version = assembly.GetName().Version;
+            if (version == null) return string.Empty;
             return string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
         }
     }

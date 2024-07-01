77e401e5e56b9ec518a60b4526584837aebeb266
rstewa <70178227+rstewa@users.noreply.github.com>
2024-07-01 13:40:46 -0500

    added try catches for #49

--- Audibly.App/Helpers/UserSettings.cs
+++ Audibly.App/Helpers/UserSettings.cs
@@ -1,8 +1,10 @@
 // Author: rstewa · https://github.com/rstewa
-// Created: 6/13/2024
-// Updated: 6/13/2024
+// Created: 06/16/2024
+// Updated: 07/01/2024
 
+using System;
 using Windows.Storage;
+using Sentry;
 
 namespace Audibly.App.Helpers;
 
@@ -12,25 +14,46 @@ public static class UserSettings
     {
         get
         {
-            var volume = ApplicationData.Current.LocalSettings.Values["Volume"];
-            if (volume != null) return (double)volume;
-            
-            ApplicationData.Current.LocalSettings.Values["Volume"] = 100;
-            return 100;
+            try
+            {
+                var volume = ApplicationData.Current.LocalSettings.Values["Volume"];
+                if (volume != null)
+                    if (double.TryParse(volume.ToString(), out var result))
+                        return result;
 
+                ApplicationData.Current.LocalSettings.Values["Volume"] = 100;
+                return 100;
+            }
+            catch (Exception e)
+            {
+                // log to sentry
+                SentrySdk.CaptureException(e);
+                return 100;
+            }
         }
         set => ApplicationData.Current.LocalSettings.Values["Volume"] = value;
     }
-    
+
     public static double PlaybackSpeed
     {
         get
         {
-            var playbackSpeed = ApplicationData.Current.LocalSettings.Values["PlaybackSpeed"];
-            if (playbackSpeed != null) return (double)playbackSpeed;
-            
-            ApplicationData.Current.LocalSettings.Values["PlaybackSpeed"] = 1;
-            return 1;
+            try
+            {
+                var playbackSpeed = ApplicationData.Current.LocalSettings.Values["PlaybackSpeed"];
+                if (playbackSpeed != null)
+                    if (double.TryParse(playbackSpeed.ToString(), out var result))
+                        return result;
+
+                ApplicationData.Current.LocalSettings.Values["PlaybackSpeed"] = 1;
+                return 1;
+            }
+            catch (Exception e)
+            {
+                // log to sentry
+                SentrySdk.CaptureException(e);
+                return 1;
+            }
         }
         set => ApplicationData.Current.LocalSettings.Values["PlaybackSpeed"] = value;
     }

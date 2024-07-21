bb4144ea291d45a320599858a39afe7509a64b17
rstewa <70178227+rstewa@users.noreply.github.com>
2024-07-21 01:43:30 -0500

    Added a powershell script that packages the .msix files into a .msixbundle file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+### Added
+
+- Added a powershell script that packages the .msix files into a .msixbundle file
+
 ## [2.0.15] - 07-21-24
 
 ### Changed
---
+++ package-and-publish.ps1
@@ -0,0 +1,49 @@
+# TODO
+# MSBuild.exe .\Audibly.sln /t:Rebuild /p:Configuration=Release /p:AppxPackageDir="Packages" /p:UapAppxPackageBuildMode=SideloadOnly /p:AppxBundle= /p:GenerateAppxPackageOnBuild=true
+
+$version = "2.0.15.0"
+$targetFramework = "net8.0-windows10.0.19041.0"
+
+$packagesDirectory = ".\Packages"
+
+if (!(Test-Path $packagesDirectory)) {
+    New-Item -ItemType Directory -Path $packagesDirectory
+}
+
+$msixDirectory = ".\Packages\msix"
+
+if (!(Test-Path $msixDirectory)) {
+    New-Item -ItemType Directory -Path $msixDirectory
+}
+
+Remove-Item -Path $msixDirectory\* -Recurse -Force
+
+$targetDirectories = @(
+    ".\Audibly.App\bin\ARM64\Release\$targetFramework\AppPackages\",
+    ".\Audibly.App\bin\x64\Release\$targetFramework\AppPackages\",
+    ".\Audibly.App\bin\x86\Release\$targetFramework\AppPackages\"
+)
+
+foreach ($targetDirectory in $targetDirectories) {
+    $versionDirectory = Get-ChildItem -Path $targetDirectory -Directory | Where-Object { $_.Name -match $version }
+    $versionDirectoryPath = $versionDirectory.FullName
+    $msixFile = Get-ChildItem -Path $versionDirectoryPath -Filter "*.msix"
+
+    Copy-Item -Path $msixFile.FullName -Destination $msixDirectory
+}
+
+# create the msixbundle
+$msixBundleDirectory = ".\Packages\msixbundle"
+
+if (!(Test-Path $msixBundleDirectory)) {
+    New-Item -ItemType Directory -Path $msixBundleDirectory
+}
+
+$msixBundleFilePath = Join-Path -Path $msixBundleDirectory -ChildPath "Audibly_$version.msixbundle"
+
+# delete the msixbundle if it already exists
+if (Test-Path $msixBundleFilePath) {
+    Remove-Item -Path $msixBundleFilePath -Force
+}
+
+MakeAppx bundle /bv $version /d $msixDirectory /p $msixBundleFilePath

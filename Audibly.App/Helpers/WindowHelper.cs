759169fef04df714f74e8fd450845503f5d182c7
rstewa <dev@audibly.info>
2025-02-26 02:06:54 -0600

    wip: light theme

--- Audibly.App/Helpers/WindowHelper.cs
+++ Audibly.App/Helpers/WindowHelper.cs
@@ -63,6 +63,8 @@ public static class WindowHelper
         var rootPage = new NewMiniPlayerPage();
         miniPlayerWindow.Content = rootPage;
 
+        ThemeHelper.Initialize();
+        
         (miniPlayerWindow as MainWindow)?.TrySetSystemBackdrop();
 
         miniPlayerWindow.SetWindowDraggable(true);
